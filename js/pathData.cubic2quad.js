37766cb0f9fd96b0e24ada4151a73a4b1140c27e
herrstrietzel <36772754+herrstrietzel@users.noreply.github.com>
2023-09-26 17:42:31 +0200

    Update pathData.cubic2quad.js
    Added arc to (quadratic & cubic) bezier helper

--- js/pathData.cubic2quad.js
+++ js/pathData.cubic2quad.js
@@ -1,3 +1,143 @@
+/**
+* convert Arcs to quadratic
+* or cubic beziers
+* recommended angle threshhold for quadratic: 22.5
+*/
+  function arcCommandToBezier(p0, comValues, quadratic=false, angleThresh = 90, recursive = false) {
+    let [r1, r2, angle, largeArcFlag, sweepFlag, x2, y2] = comValues;
+    let [x1, y1] = [p0.x, p0.y];
+    const interpolate = (p1x, p1y, p2x, p2y, t) => {
+      let pt = {
+        x: (p2x - p1x) * t + p1x,
+        y: (p2y - p1y) * t + p1y
+      };
+      return pt
+    }
+    const degToRad = (degrees) => {
+      return (Math.PI * degrees) / 180;
+    };
+    const rotate = (x, y, angleRad) => {
+      let X = x * Math.cos(angleRad) - y * Math.sin(angleRad);
+      let Y = x * Math.sin(angleRad) + y * Math.cos(angleRad);
+      return {
+        x: X,
+        y: Y
+      };
+    };
+    let angleRad = degToRad(angle);
+    let params = [];
+    let x, y, f1, f2, cx, cy, h;
+    if (recursive) {
+      f1 = recursive[0];
+      f2 = recursive[1];
+      cx = recursive[2];
+      cy = recursive[3];
+    } else {
+      let p1 = rotate(x1, y1, -angleRad);
+      x1 = p1.x;
+      y1 = p1.y;
+      let p2 = rotate(x2, y2, -angleRad);
+      x2 = p2.x;
+      y2 = p2.y;
+      x = (x1 - x2) / 2;
+      y = (y1 - y2) / 2;
+      h = (x * x) / (r1 * r1) + (y * y) / (r2 * r2);
+      if (h > 1) {
+        h = Math.sqrt(h);
+        r1 = h * r1;
+        r2 = h * r2;
+      }
+      let sign = largeArcFlag === sweepFlag ? -1 : 1;
+      let r1Pow = r1 * r1;
+      let r2Pow = r2 * r2;
+      let left = r1Pow * r2Pow - r1Pow * y * y - r2Pow * x * x;
+      let right = r1Pow * y * y + r2Pow * x * x;
+      let k = sign * Math.sqrt(Math.abs(left / right));
+      cx = (k * r1 * y) / r2 + (x1 + x2) / 2;
+      cy = (k * -r2 * x) / r1 + (y1 + y2) / 2;
+      f1 = Math.asin(parseFloat(((y1 - cy) / r2).toFixed(9)));
+      f2 = Math.asin(parseFloat(((y2 - cy) / r2).toFixed(9)));
+      if (x1 < cx) {
+        f1 = Math.PI - f1;
+      }
+      if (x2 < cx) {
+        f2 = Math.PI - f2;
+      }
+      if (f1 < 0) {
+        f1 = Math.PI * 2 + f1;
+      }
+      if (f2 < 0) {
+        f2 = Math.PI * 2 + f2;
+      }
+      if (sweepFlag && f1 > f2) {
+        f1 = f1 - Math.PI * 2;
+      }
+      if (!sweepFlag && f2 > f1) {
+        f2 = f2 - Math.PI * 2;
+      }
+    }
+    let df = f2 - f1;
+    if (Math.abs(df) > (Math.PI * angleThresh) / 180) {
+      let f2old = f2;
+      let x2old = x2;
+      let y2old = y2;
+      f2 =
+        sweepFlag && f2 > f1 ?
+        (f2 = f1 + ((Math.PI * angleThresh) / 180) * 1) :
+        (f2 = f1 + ((Math.PI * angleThresh) / 180) * -1);
+      x2 = cx + r1 * Math.cos(f2);
+      y2 = cy + r2 * Math.sin(f2);
+      params = arcCommandToBezier({
+          x: x2,
+          y: y2
+        },
+        [r1, r2, angle, 0, sweepFlag, x2old, y2old],
+quadratic,
+        angleThresh,
+        [f2, f2old, cx, cy]
+      );
+    }
+    df = f2 - f1;
+    let c1 = Math.cos(f1);
+    let s1 = Math.sin(f1);
+    let c2 = Math.cos(f2);
+    let s2 = Math.sin(f2);
+    let t = Math.tan(df / 4);
+    let hx = (4 / 3) * r1 * t;
+    let hy = (4 / 3) * r2 * t;
+    let m1 = [x1, y1];
+    let m2 = [x1 + hx * s1, y1 - hy * c1];
+    let m3 = [x2 + hx * s2, y2 - hy * c2];
+    let m4 = [x2, y2];
+    m2[0] = 2 * m1[0] - m2[0];
+    m2[1] = 2 * m1[1] - m2[1];
+    if (recursive) {
+      return [m2, m3, m4].concat(params);
+    } else {
+      params = [m2, m3, m4].concat(params);
+      let commands = [];
+      for (var i = 0; i < params.length; i += 3) {
+        r1 = rotate(params[i][0], params[i][1], angleRad);
+        r2 = rotate(params[i + 1][0], params[i + 1][1], angleRad);
+        r3 = rotate(params[i + 2][0], params[i + 2][1], angleRad);
+        let cp1Q = interpolate(r3.x, r3.y, r2.x, r2.y, 3 / 2)
+        if(quadratic){
+          commands.push({
+            type: 'Q',
+            values: [cp1Q.x, cp1Q.y, r3.x, r3.y]
+          })          
+        }else{
+          commands.push({
+            type: "C",
+            values: [r1.x, r1.y, r2.x, r2.y, r3.x, r3.y]
+          });
+        }
+      }
+      return commands;
+    }
+  }
+
+
 
 /**
  * fontello/cubic2quad
