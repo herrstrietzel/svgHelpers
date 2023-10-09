015b418b8f73844f55d79f6e52832dbc0289d5c2
herrstrietzel <36772754+herrstrietzel@users.noreply.github.com>
2023-10-10 00:58:29 +0200

    Update getPointAtLengthLookup.js
    cleanup

--- js/getPointAtLengthLookup.js
+++ js/getPointAtLengthLookup.js
@@ -46,7 +46,6 @@ function getPointAtLengthLookup(pathLengthLookup, length) {
                     /**
                      *  cubic or quadratic beziers
                     */
-                    let isLine = false;
                     let l1 = getLineLength(points[0], points[1]) + getLineLength(points[1], points[2]);
                     let l2 = type === 'C' ? getLineLength(points[2], points[3]) : 0;
                     let lBase = getLineLength(points[0], points[points.length - 1]);
@@ -56,7 +55,6 @@ function getPointAtLengthLookup(pathLengthLookup, length) {
                         diffLength = end - length;
                         newT = 1 - (1 / total) * diffLength;
                         pt = newT <= 1 ? interpolatedPoint(points[0], points[2], newT) : points[points.length - 1];
-                        isLine = true;
                     }
 
                     // is curve
@@ -174,11 +172,8 @@ function getPAthLengthLookup(d, tDivisions = 10) {
         }
         lengthLookup.totalLength = pathLength;
     }
-
     return lengthLookup;
 }
-
-
 
 /**
  * Based on snap.svg bezlen() function
@@ -209,7 +204,6 @@ function cubicBezierLength(p0, cp1, cp2, p, t = 1) {
     return t2 * sum;
 }
 
-
 function quadraticBezierLength(p0, cp1, p, t = 1) {
     if (t === 0) {
         return 0;
@@ -238,7 +232,6 @@ function quadraticBezierLength(p0, cp1, p, t = 1) {
     }
 
     let a, b, c, d, e, e1, d1, v1x, v1y;
-
     v1x = cp1.x * 2;
     v1y = cp1.y * 2;
     d = p0.x - v1x + p.x;
@@ -263,14 +256,11 @@ function quadraticBezierLength(p0, cp1, p, t = 1) {
     );
 }
 
-
 function getLineLength(p1, p2) {
     return Math.sqrt(
         (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y)
     );
 }
-
-
 
 /**
  * Linear  interpolation (LERP) helper
@@ -311,7 +301,6 @@ function getPointAtCubicSegmentT(p0, cp1, cp2, p, t = 0.5) {
     };
 }
 
-
 function getPointAtQuadraticSegmentT(p0, cp1, p, t = 0.5) {
     let t1 = 1 - t;
     return {
@@ -319,8 +308,6 @@ function getPointAtQuadraticSegmentT(p0, cp1, p, t = 0.5) {
         y: t1 * t1 * p0.y + 2 * t1 * t * cp1.y + t ** 2 * p.y
     };
 }
-
-
 
 /**
  * parse pathdata normalized
@@ -336,18 +323,17 @@ function getPathDataNorm(d) {
     if (hasShorthands) {
         pathData = pathDataToLonghands(pathData);
     }
-    
+
     if (hasArcsOrQuadratic) {
         pathData = pathDataToCubic(pathData, false);
     }
     return pathData;
 }
 
-
 /**
  * normalize to all absolute, cubic, no shorthand
  */
-function pathDataToCubic(pathData, convertQuadratic=true) {
+function pathDataToCubic(pathData, convertQuadratic = true) {
     let pathDataNorm = [];
     pathData.forEach((com, i) => {
         let [type, values] = [com.type, com.values];
@@ -366,7 +352,7 @@ function pathDataToCubic(pathData, convertQuadratic=true) {
             });
         }
 
-        else if(type === "Q" && convertQuadratic){
+        else if (type === "Q" && convertQuadratic) {
             pathDataNorm.push(pathDataQuadratic2Cubic(p0, values));
         }
 
@@ -374,10 +360,8 @@ function pathDataToCubic(pathData, convertQuadratic=true) {
             pathDataNorm.push(com);
         }
     });
-
     return pathDataNorm;
 }
-
 
 /**
  * Based on: jkroso/parse-svg-path
@@ -386,7 +370,7 @@ function pathDataToCubic(pathData, convertQuadratic=true) {
  * of commands where each command is an Array of the
  * form `{type:command, values:[]}`
  */
-function getPathData(path) {
+function getPathData(d) {
     // expected argument lengths
     let length = {
         a: 7, c: 6, h: 1, l: 2, m: 2,
@@ -401,8 +385,8 @@ function getPathData(path) {
         return numbers ? numbers.map(Number) : [];
     };
     // split adjacent zero values like 0 05 => 0 0 5
-    path = path.replace(/( )(0)(\d+)/g, "$1 $2 $3");
-    path.replace(segment, function (_, command, args) {
+    d = d.replace(/( )(0)(\d+)/g, "$1 $2 $3");
+    d.replace(segment, function (_, command, args) {
         let type = command.toLowerCase();
         args = parseValues(args);
         // implicit lineto
@@ -586,99 +570,99 @@ function pathDataToLonghands(pathData) {
 * or cubic beziers
 * recommended angle threshhold for quadratic: 22.5
 */
-function arcCommandToBezier(p0, comValues, quadratic=false, angleThresh = 90, recursive = false) {
+function arcCommandToBezier(p0, comValues, quadratic = false, angleThresh = 90, recursive = false) {
     let [r1, r2, angle, largeArcFlag, sweepFlag, x2, y2] = comValues;
     let [x1, y1] = [p0.x, p0.y];
     const interpolate = (p1x, p1y, p2x, p2y, t) => {
-      let pt = {
-        x: (p2x - p1x) * t + p1x,
-        y: (p2y - p1y) * t + p1y
-      };
-      return pt
+        let pt = {
+            x: (p2x - p1x) * t + p1x,
+            y: (p2y - p1y) * t + p1y
+        };
+        return pt
     }
     const degToRad = (degrees) => {
-      return (Math.PI * degrees) / 180;
+        return (Math.PI * degrees) / 180;
     };
     const rotate = (x, y, angleRad) => {
-      let X = x * Math.cos(angleRad) - y * Math.sin(angleRad);
-      let Y = x * Math.sin(angleRad) + y * Math.cos(angleRad);
-      return {
-        x: X,
-        y: Y
-      };
+        let X = x * Math.cos(angleRad) - y * Math.sin(angleRad);
+        let Y = x * Math.sin(angleRad) + y * Math.cos(angleRad);
+        return {
+            x: X,
+            y: Y
+        };
     };
     let angleRad = degToRad(angle);
     let params = [];
     let x, y, f1, f2, cx, cy, h;
     if (recursive) {
-      f1 = recursive[0];
-      f2 = recursive[1];
-      cx = recursive[2];
-      cy = recursive[3];
+        f1 = recursive[0];
+        f2 = recursive[1];
+        cx = recursive[2];
+        cy = recursive[3];
     } else {
-      let p1 = rotate(x1, y1, -angleRad);
-      x1 = p1.x;
-      y1 = p1.y;
-      let p2 = rotate(x2, y2, -angleRad);
-      x2 = p2.x;
-      y2 = p2.y;
-      x = (x1 - x2) / 2;
-      y = (y1 - y2) / 2;
-      h = (x * x) / (r1 * r1) + (y * y) / (r2 * r2);
-      if (h > 1) {
-        h = Math.sqrt(h);
-        r1 = h * r1;
-        r2 = h * r2;
-      }
-      let sign = largeArcFlag === sweepFlag ? -1 : 1;
-      let r1Pow = r1 * r1;
-      let r2Pow = r2 * r2;
-      let left = r1Pow * r2Pow - r1Pow * y * y - r2Pow * x * x;
-      let right = r1Pow * y * y + r2Pow * x * x;
-      let k = sign * Math.sqrt(Math.abs(left / right));
-      cx = (k * r1 * y) / r2 + (x1 + x2) / 2;
-      cy = (k * -r2 * x) / r1 + (y1 + y2) / 2;
-      f1 = Math.asin(parseFloat(((y1 - cy) / r2).toFixed(9)));
-      f2 = Math.asin(parseFloat(((y2 - cy) / r2).toFixed(9)));
-      if (x1 < cx) {
-        f1 = Math.PI - f1;
-      }
-      if (x2 < cx) {
-        f2 = Math.PI - f2;
-      }
-      if (f1 < 0) {
-        f1 = Math.PI * 2 + f1;
-      }
-      if (f2 < 0) {
-        f2 = Math.PI * 2 + f2;
-      }
-      if (sweepFlag && f1 > f2) {
-        f1 = f1 - Math.PI * 2;
-      }
-      if (!sweepFlag && f2 > f1) {
-        f2 = f2 - Math.PI * 2;
-      }
+        let p1 = rotate(x1, y1, -angleRad);
+        x1 = p1.x;
+        y1 = p1.y;
+        let p2 = rotate(x2, y2, -angleRad);
+        x2 = p2.x;
+        y2 = p2.y;
+        x = (x1 - x2) / 2;
+        y = (y1 - y2) / 2;
+        h = (x * x) / (r1 * r1) + (y * y) / (r2 * r2);
+        if (h > 1) {
+            h = Math.sqrt(h);
+            r1 = h * r1;
+            r2 = h * r2;
+        }
+        let sign = largeArcFlag === sweepFlag ? -1 : 1;
+        let r1Pow = r1 * r1;
+        let r2Pow = r2 * r2;
+        let left = r1Pow * r2Pow - r1Pow * y * y - r2Pow * x * x;
+        let right = r1Pow * y * y + r2Pow * x * x;
+        let k = sign * Math.sqrt(Math.abs(left / right));
+        cx = (k * r1 * y) / r2 + (x1 + x2) / 2;
+        cy = (k * -r2 * x) / r1 + (y1 + y2) / 2;
+        f1 = Math.asin(parseFloat(((y1 - cy) / r2).toFixed(9)));
+        f2 = Math.asin(parseFloat(((y2 - cy) / r2).toFixed(9)));
+        if (x1 < cx) {
+            f1 = Math.PI - f1;
+        }
+        if (x2 < cx) {
+            f2 = Math.PI - f2;
+        }
+        if (f1 < 0) {
+            f1 = Math.PI * 2 + f1;
+        }
+        if (f2 < 0) {
+            f2 = Math.PI * 2 + f2;
+        }
+        if (sweepFlag && f1 > f2) {
+            f1 = f1 - Math.PI * 2;
+        }
+        if (!sweepFlag && f2 > f1) {
+            f2 = f2 - Math.PI * 2;
+        }
     }
     let df = f2 - f1;
     if (Math.abs(df) > (Math.PI * angleThresh) / 180) {
-      let f2old = f2;
-      let x2old = x2;
-      let y2old = y2;
-      f2 =
-        sweepFlag && f2 > f1 ?
-        (f2 = f1 + ((Math.PI * angleThresh) / 180) * 1) :
-        (f2 = f1 + ((Math.PI * angleThresh) / 180) * -1);
-      x2 = cx + r1 * Math.cos(f2);
-      y2 = cy + r2 * Math.sin(f2);
-      params = arcCommandToBezier({
-          x: x2,
-          y: y2
+        let f2old = f2;
+        let x2old = x2;
+        let y2old = y2;
+        f2 =
+            sweepFlag && f2 > f1 ?
+                (f2 = f1 + ((Math.PI * angleThresh) / 180) * 1) :
+                (f2 = f1 + ((Math.PI * angleThresh) / 180) * -1);
+        x2 = cx + r1 * Math.cos(f2);
+        y2 = cy + r2 * Math.sin(f2);
+        params = arcCommandToBezier({
+            x: x2,
+            y: y2
         },
-        [r1, r2, angle, 0, sweepFlag, x2old, y2old],
-quadratic,
-        angleThresh,
-        [f2, f2old, cx, cy]
-      );
+            [r1, r2, angle, 0, sweepFlag, x2old, y2old],
+            quadratic,
+            angleThresh,
+            [f2, f2old, cx, cy]
+        );
     }
     df = f2 - f1;
     let c1 = Math.cos(f1);
@@ -695,34 +679,34 @@ quadratic,
     m2[0] = 2 * m1[0] - m2[0];
     m2[1] = 2 * m1[1] - m2[1];
     if (recursive) {
-      return [m2, m3, m4].concat(params);
+        return [m2, m3, m4].concat(params);
     } else {
-      params = [m2, m3, m4].concat(params);
-      let commands = [];
-      for (var i = 0; i < params.length; i += 3) {
-        r1 = rotate(params[i][0], params[i][1], angleRad);
-        r2 = rotate(params[i + 1][0], params[i + 1][1], angleRad);
-        r3 = rotate(params[i + 2][0], params[i + 2][1], angleRad);
-        let cp1Q = interpolate(r3.x, r3.y, r2.x, r2.y, 3 / 2)
-        if(quadratic){
-          commands.push({
-            type: 'Q',
-            values: [cp1Q.x, cp1Q.y, r3.x, r3.y]
-          })          
-        }else{
-          commands.push({
-            type: "C",
-            values: [r1.x, r1.y, r2.x, r2.y, r3.x, r3.y]
-          });
+        params = [m2, m3, m4].concat(params);
+        let commands = [];
+        for (var i = 0; i < params.length; i += 3) {
+            r1 = rotate(params[i][0], params[i][1], angleRad);
+            r2 = rotate(params[i + 1][0], params[i + 1][1], angleRad);
+            r3 = rotate(params[i + 2][0], params[i + 2][1], angleRad);
+            let cp1Q = interpolate(r3.x, r3.y, r2.x, r2.y, 3 / 2)
+            if (quadratic) {
+                commands.push({
+                    type: 'Q',
+                    values: [cp1Q.x, cp1Q.y, r3.x, r3.y]
+                })
+            } else {
+                commands.push({
+                    type: "C",
+                    values: [r1.x, r1.y, r2.x, r2.y, r3.x, r3.y]
+                });
+            }
         }
-      }
-      return commands;
+        return commands;
     }
-  }
+}
 
-  /**
- * convert quadratic commands to cubic
- */
+/**
+* convert quadratic commands to cubic
+*/
 function pathDataQuadratic2Cubic(p0, com) {
     if (Array.isArray(p0)) {
         p0 = {
