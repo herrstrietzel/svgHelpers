cc774bf2b3cb4942d058bc5dc0bb57969c405c74
herrstrietzel <36772754+herrstrietzel@users.noreply.github.com>
2024-01-04 21:28:33 +0100

    Update pathDataGetBBox.js
    Prevent calculations if controlpoints in inner bbox

--- js/pathDataGetBBox.js
+++ js/pathDataGetBBox.js
@@ -20,6 +20,21 @@ function getPathDataBBox(pathData) {
     let yMin = Infinity;
     let yMax = -Infinity;
 
+    const setXYmaxMin = (pt) => {
+        if (pt.x < xMin) {
+            xMin = pt.x
+        }
+        if (pt.x > xMax) {
+            xMax = pt.x
+        }
+        if (pt.y < yMin) {
+            yMin = pt.y
+        }
+        if (pt.y > yMax) {
+            yMax = pt.y
+        }
+    }
+
     for (let i = 0; i < pathData.length; i++) {
         let com = pathData[i]
         let { type, values } = com;
@@ -33,18 +48,8 @@ function getPathDataBBox(pathData) {
             let p = { x: values[valuesL - 2], y: values[valuesL - 1] };
 
             // add final on path point
-            if (p.x < xMin) {
-                xMin = p.x
-            }
-            if (p.x > xMax) {
-                xMax = p.x
-            }
-            if (p.y < yMin) {
-                yMin = p.y
-            }
-            if (p.y > yMax) {
-                yMax = p.y
-            }
+            setXYmaxMin(p)
+
 
             if (type === 'C' || type === 'Q') {
                 let cp1 = { x: values[0], y: values[1] };
@@ -54,36 +59,15 @@ function getPathDataBBox(pathData) {
                 let bezierExtremesT = getBezierExtremeT(pts)
                 bezierExtremesT.forEach(t => {
                     let pt = getPointAtBezierT(pts, t);
-                    if (pt.x < xMin) {
-                        xMin = pt.x
-                    }
-                    if (pt.x > xMax) {
-                        xMax = pt.x
-                    }
-                    if (pt.y < yMin) {
-                        yMin = pt.y
-                    }
-                    if (pt.y > yMax) {
-                        yMax = pt.y
-                    }
+                    setXYmaxMin(pt)
                 })
+
             }
 
             else if (type === 'A') {
                 let arcExtremes = getArcExtemes(p0, values)
                 arcExtremes.forEach(pt => {
-                    if (pt.x < xMin) {
-                        xMin = pt.x
-                    }
-                    if (pt.x > xMax) {
-                        xMax = pt.x
-                    }
-                    if (pt.y < yMin) {
-                        yMin = pt.y
-                    }
-                    if (pt.y > yMax) {
-                        yMax = pt.y
-                    }
+                    setXYmaxMin(pt)
                 })
             }
         }
@@ -118,6 +102,8 @@ function getArcExtemes(p0, values) {
 
     //parametrize arcto data
     let arcData = svgArcToCenterParam(p0.x, p0.y, values[0], values[1], values[2], values[3], values[4], values[5], values[6]);
+
+
     let { rx, ry, pt, endAngle, deltaAngle } = arcData;
 
     // arc rotation
@@ -147,19 +133,6 @@ function getArcExtemes(p0, values) {
     let angle4 = angle3 + Math.PI;
 
 
-    // get point for this theta
-    p1 = arc(angle1, cx, cy, rx, ry, alpha);
-
-    // get anti-symmetric point
-    p2 = arc(angle2, cx, cy, rx, ry, alpha);
-
-    // get point for this theta
-    p3 = arc(angle3, cx, cy, rx, ry, alpha);
-
-    // get anti-symmetric point
-    p4 = arc(angle4, cx, cy, rx, ry, alpha);
-
-
     // inner bounding box
     let xArr = [p0.x, p.x]
     let yArr = [p0.y, p.y]
@@ -185,23 +158,31 @@ function getArcExtemes(p0, values) {
      * otherwise exclude elliptic extreme points
     */
 
+    // right
+    if (pP2.x > xMax || pP3.x > xMax) {
+        // get point for this theta
+        p1 = arc(angle1, cx, cy, rx, ry, alpha);
+        extremes.push(p1)
+    }
+
     // left
     if (pP2.x < xMin || pP3.x < xMin) {
+        // get anti-symmetric point
+        p2 = arc(angle2, cx, cy, rx, ry, alpha);
         extremes.push(p2)
     }
 
     // top
     if (pP2.y < yMin || pP3.y < yMin) {
+        // get anti-symmetric point
+        p4 = arc(angle4, cx, cy, rx, ry, alpha);
         extremes.push(p4)
-    }
-
-    // right
-    if (pP2.x > xMax || pP3.x > xMax) {
-        extremes.push(p1)
     }
 
     // bottom
     if (pP2.y > yMax || pP3.y > yMax) {
+        // get point for this theta
+        p3 = arc(angle3, cx, cy, rx, ry, alpha);
         extremes.push(p3)
     }
 
@@ -331,9 +312,29 @@ function getBezierExtremeT(pts) {
 
 
 // cubic bezier.
-//(x0,y0) is start point; (x1,y1),(x2,y2) is control points; (x3,y3) is end point.
 function cubicBezierExtremeT(p0, cp1, cp2, p) {
     let [x0, y0, x1, y1, x2, y2, x3, y3] = [p0.x, p0.y, cp1.x, cp1.y, cp2.x, cp2.y, p.x, p.y];
+
+    /**
+     * if control points are within 
+     * bounding box of start and end point 
+     * we cant't have extremes
+     */
+    let top = Math.min(p0.y, p.y)
+    let left = Math.min(p0.x, p.x)
+    let right = Math.max(p0.x, p.x)
+    let bottom = Math.max(p0.y, p.y)
+
+    if (
+        cp1.y >= top && cp1.y <= bottom &&
+        cp2.y >= top && cp2.y <= bottom &&
+        cp1.x >= left && cp1.x <= right &&
+        cp2.x >= left && cp2.x <= right
+    ) {
+        //console.log('no extreme cubic');
+        return []
+    }
+
     var tArr = [],
         a, b, c, t, t1, t2, b2ac, sqrt_b2ac;
     for (var i = 0; i < 2; ++i) {
@@ -388,6 +389,26 @@ function cubicBezierExtremeT(p0, cp1, cp2, p) {
 //For quadratic bezier.
 //(x0,y0) is start point; (x1,y1) is control points; (x2,y2) is end point.
 function quadraticBezierExtremeT(p0, cp1, p) {
+
+    /**
+     * if control points are within 
+     * bounding box of start and end point 
+     * we cant't have extremes
+     */
+    let top = Math.min(p0.y, p.y)
+    let left = Math.min(p0.x, p.x)
+    let right = Math.max(p0.x, p.x)
+    let bottom = Math.max(p0.y, p.y)
+
+    if (
+        cp1.y >= top && cp1.y <= bottom &&
+        cp1.x >= left && cp1.x <= right
+    ) {
+        //console.log('no extreme quadratic');
+        return []
+    }
+
+
     let [x0, y0, x1, y1, x2, y2] = [p0.x, p0.y, cp1.x, cp1.y, p.x, p.y];
     let extemeT = [];
 
@@ -404,309 +425,3 @@ function quadraticBezierExtremeT(p0, cp1, p) {
     }
     return extemeT
 }
-
-/**
- * parse pathData from d attribute
- * the core function to parse the pathData array from a d string
- **/
-function parseDtoPathData(d) {
-  let dClean = d
-    // remove new lines and tabs
-    .replace(/[\n\r\t]/g, "")
-    // replace comma with space
-    .replace(/,/g, " ")
-    // add space before minus sign
-    .replace(/(\d+)(\-)/g, "$1 $2")
-    // decompose multiple adjacent decimal delimiters like 0.5.5.5 => 0.5 0.5 0.5
-    .replace(/(\.)(?=(\d+\.\d+)+)(\d+)/g, "$1$3 ")
-    // add new lines before valid command letters
-    .replace(/([mlcsqtahvz])/gi, "\n$1 ")
-    // remove duplicate whitespace
-    .replace(/\ {2,}/g, " ")
-    // remove whitespace from right and left
-    .trim();
-
-  // split commands
-  let commands = dClean.split("\n").map((val) => {
-    return val.trim();
-  });
-
-  // compile pathData
-  let pathData = [];
-  let comLengths = {
-    m: 2,
-    a: 7,
-    c: 6,
-    h: 1,
-    l: 2,
-    q: 4,
-    s: 4,
-    t: 2,
-    v: 1,
-    z: 0
-  };
-  let errors = [];
-
-  // normalize convatenated larceArc and sweep flags
-  const unravelArcValues = (values) => {
-    let chunksize = 7,
-      n = 0,
-      arcComs = [];
-    for (let i = 0; i < values.length; i++) {
-      let com = values[i];
-
-      // reset counter
-      if (n >= chunksize) {
-        n = 0;
-      }
-      // if 3. or 4. parameter longer than 1
-      if ((n === 3 || n === 4) && com.length > 1) {
-        let largeArc = n === 3 ? com.substring(0, 1) : "";
-        let sweep = n === 3 ? com.substring(1, 2) : com.substring(0, 1);
-        let finalX = n === 3 ? com.substring(2) : com.substring(1);
-        let comN = [largeArc, sweep, finalX].filter(Boolean);
-        arcComs.push(comN);
-        n += comN.length;
-      } else {
-        // regular
-        arcComs.push(com);
-        n++;
-      }
-    }
-    return arcComs.flat().filter(Boolean);
-  };
-
-  for (let i = 0; i < commands.length; i++) {
-    let com = commands[i].split(" ");
-    let type = com.shift();
-    let typeRel = type.toLowerCase();
-    let isRel = type === typeRel;
-
-    /**
-     * large arc and sweep flags
-     * are boolean and can be concatenated like
-     * 11 or 01
-     * or be concatenated with the final on path points like
-     * 1110 10 => 1 1 10 10
-     */
-    if (typeRel === "a") {
-      com = unravelArcValues(com);
-    }
-
-    // convert to numbers
-    let values = com.map((val) => {
-      return parseFloat(val);
-    });
-
-    // if string contains repeated shorthand commands - split them
-    let chunkSize = comLengths[typeRel];
-    let chunk = values.slice(0, chunkSize);
-    pathData.push({ type: type, values: chunk });
-
-    // too few values
-    if (chunk.length < chunkSize) {
-      errors.push(
-        `${i}. command (${type}) has ${chunk.length}/${chunkSize} values - ${
-          chunkSize - chunk.length
-        } too few`
-      );
-    }
-
-    // has implicit commands
-    if (values.length > chunkSize) {
-      let typeImplicit = typeRel === "m" ? (isRel ? "l" : "L") : type;
-      for (let i = chunkSize; i < values.length; i += chunkSize) {
-        let chunk = values.slice(i, i + chunkSize);
-        pathData.push({ type: typeImplicit, values: chunk });
-        if (chunk.length !== chunkSize) {
-          errors.push(
-            `${i}. command (${type}) has ${
-              chunk.length + chunkSize
-            }/${chunkSize} - ${chunk.length} values too many `
-          );
-        }
-      }
-    }
-  }
-  if (errors.length) {
-    console.log(errors);
-  }
-
-  /**
-   * first M is always absolute/uppercase -
-   * unless it adds relative linetos
-   * (facilitates d concatenating)
-   */
-  pathData[0].type = "M";
-  return pathData;
-}
-
-/**
- * converts all commands to absolute
- */
-
-function pathDataToLonghands(pathData) {
-  let pathDataAbs = [];
-  let offX = pathData[0].values[0];
-  let offY = pathData[0].values[1];
-  let lastX = offX;
-  let lastY = offY;
-
-  // analyze pathdata
-  let commandTokens = pathData
-    .map((com) => {
-      return com.type;
-    })
-    .join("");
-  let hasShorthands = /[hstv]/gi.test(commandTokens);
-
-  pathData.forEach((com, i) => {
-    let { type, values } = com;
-    let typeRel = type.toLowerCase();
-    let typeAbs = type.toUpperCase();
-    let valuesL = values.length;
-    let isRelative = type === typeRel;
-    let comPrev = i > 0 ? pathData[i - 1] : pathData[0];
-    let valuesPrev = comPrev.values;
-    let valuesPrevL = valuesPrev.length;
-
-    if (isRelative) {
-      com.type = typeAbs;
-
-      switch (typeRel) {
-        case "a":
-          com.values = [
-            values[0],
-            values[1],
-            values[2],
-            values[3],
-            values[4],
-            values[5] + offX,
-            values[6] + offY
-          ];
-          break;
-
-        case "h":
-        case "v":
-          com.values = type === "h" ? [values[0] + offX] : [values[0] + offY];
-          break;
-
-        case "m":
-        case "l":
-        case "t":
-          com.values = [values[0] + offX, values[1] + offY];
-          break;
-
-        case "c":
-          com.values = [
-            values[0] + offX,
-            values[1] + offY,
-            values[2] + offX,
-            values[3] + offY,
-            values[4] + offX,
-            values[5] + offY
-          ];
-          break;
-
-        case "q":
-        case "s":
-          com.values = [
-            values[0] + offX,
-            values[1] + offY,
-            values[2] + offX,
-            values[3] + offY
-          ];
-          break;
-      }
-    }
-    // is absolute
-    else {
-      offX = 0;
-      offY = 0;
-    }
-
-    /**
-     * convert shorthands
-     */
-    if (hasShorthands) {
-      let cp1X, cp1Y, cpN1X, cpN1Y, cp2X, cp2Y;
-      if (com.type === "H" || com.type === "V") {
-        com.values =
-          com.type === "H" ? [com.values[0], lastY] : [lastX, com.values[0]];
-        com.type = "L";
-      } else if (com.type === "T" || com.type === "S") {
-        [cp1X, cp1Y] = [valuesPrev[0], valuesPrev[1]];
-        [cp2X, cp2Y] =
-          valuesPrevL > 2
-            ? [valuesPrev[2], valuesPrev[3]]
-            : [valuesPrev[0], valuesPrev[1]];
-
-        // new control point
-        cpN1X = com.type === "T" ? lastX + (lastX - cp1X) : 2 * lastX - cp2X;
-        cpN1Y = com.type === "T" ? lastY + (lastY - cp1Y) : 2 * lastY - cp2Y;
-
-        com.values = [cpN1X, cpN1Y, com.values].flat();
-        com.type = com.type === "T" ? "Q" : "C";
-      }
-    }
-
-    // add command
-    pathDataAbs.push(com);
-
-    // update offsets
-    lastX =
-      valuesL > 1
-        ? values[valuesL - 2] + offX
-        : typeRel === "h"
-        ? values[0] + offX
-        : lastX;
-    lastY =
-      valuesL > 1
-        ? values[valuesL - 1] + offY
-        : typeRel === "v"
-        ? values[0] + offY
-        : lastY;
-    offX = lastX;
-    offY = lastY;
-  });
-
-  return pathDataAbs;
-}
-
-/**
- * Linear  interpolation (LERP) helper
- */
-function interpolatedPoint(p1, p2, t = 0.5) {
-  return {
-    x: (p2.x - p1.x) * t + p1.x,
-    y: (p2.y - p1.y) * t + p1.y
-  };
-}
-
-/**
- * calculate single points on segments
- */
-function getPointAtCubicSegmentT(p0, cp1, cp2, p, t = 0.5) {
-  let t1 = 1 - t;
-  return {
-    x:
-      t1 ** 3 * p0.x +
-      3 * t1 ** 2 * t * cp1.x +
-      3 * t1 * t ** 2 * cp2.x +
-      t ** 3 * p.x,
-    y:
-      t1 ** 3 * p0.y +
-      3 * t1 ** 2 * t * cp1.y +
-      3 * t1 * t ** 2 * cp2.y +
-      t ** 3 * p.y
-  };
-}
-
-function getPointAtQuadraticSegmentT(p0, cp1, p, t = 0.5) {
-  let t1 = 1 - t;
-  return {
-    x: t1 * t1 * p0.x + 2 * t1 * t * cp1.x + t ** 2 * p.x,
-    y: t1 * t1 * p0.y + 2 * t1 * t * cp1.y + t ** 2 * p.y
-  };
-}
-
