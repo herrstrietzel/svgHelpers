f0d78d5dd67d2811ccf06327c64e48ea559b07f3
herrstrietzel <36772754+herrstrietzel@users.noreply.github.com>
2024-01-17 22:19:53 +0100

    Update parsepathDataNormalized.js

--- js/parsepathDataNormalized.js
+++ js/parsepathDataNormalized.js
@@ -12,6 +12,8 @@ let options = {
 }
 let d = "M 0 0 ... z"
 let pathDataAbs = parsepathDataNormalized(d, options)
+
+* stringify to d attribute via pathDataToD(pathData, decimals, minify) 
 */
 
 function parsepathDataNormalized(d, options = {}) {
@@ -458,5 +460,46 @@ function parsepathDataNormalized(d, options = {}) {
         }
         return pathDataArc;
     }
+}
+
+
+/**
+ * serialize pathData array to 
+ * d attribute string 
+ */
+function pathDataToD(pathData, decimals = -1, minify = false) {
+    // implicit l command
+    if (pathData[1].type === "l" && minify) {
+        pathData[0].type = "m";
+    }
+    let d = `${pathData[0].type}${pathData[0].values.join(" ")}`;
+    for (let i = 1; i < pathData.length; i++) {
+        let com0 = pathData[i - 1];
+        let com = pathData[i];
+
+        let type = (com0.type === com.type && minify) ?
+            " " : (
+                (com0.type === "m" && com.type === "l") ||
+                (com0.type === "M" && com.type === "l") ||
+                (com0.type === "M" && com.type === "L")
+            ) && minify ?
+                " " : com.type;
+
+        // round
+        if (com.values.length && decimals >= 0) {
+            com.values = com.values.map(val => { return +val.toFixed(decimals) })
+        }
+        d += `${type}${com.values.join(" ")}`;
+    }
+
+    if (minify) {
+        d = d
+            .replaceAll(" 0.", " .")
+            .replaceAll(" -", "-")
+            .replaceAll("-0.", "-.")
+            .replace(/\s+([mlcsqtahvz])/gi, "$1")
+            .replaceAll("Z", "z");
+    }
+    return d;
 }
 
